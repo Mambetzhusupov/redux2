c2f6cc71a9f3474996b60e2c112cea0df1c177db
Mambetzhusupov <150808947+Mambetzhusupov@users.noreply.github.com>
2024-02-10 18:48:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inpValue, setInpValue] = useState("");
+    const value = useSelector(state => state)
+
+    const dispatch = useDispatch()
+
+    const calculate = (e) => {
+        switch (e.target.innerText) {
+            case "+":
+                return dispatch({type: "PLUS", payload: Number(inpValue)})
+            case "-":
+                return dispatch({type: "MINUS", payload: Number(inpValue)})
+            case "*":
+                return dispatch({type: "MULTIPLY", payload: Number(inpValue)})
+            case "/":
+                return dispatch({type: "DIVIDE", payload: Number(inpValue)})
+            default:
+                break;
+        }
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <input value={inpValue} onChange={(e) => setInpValue(e.target.value)} type="number"/>
+                <button onClick={() => {
+                    dispatch({type: "CLEAR"})
+                }}>clear
+                </button>
+            </div>
+            <button onClick={(e) => calculate(e)}>+</button>
+            <button onClick={(e) => calculate(e)}>-</button>
+            <button onClick={(e) => calculate(e)}>*</button>
+            <button onClick={(e) => calculate(e)}>/</button>
+            <ul>
+                {value.list.map((item, index) => (<li key={index}>{item}</li>))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
